3fb609456deb35a2198361fc43aa9d320a05be7b
Nathan Skiles <nateskiles94@gmail.com>
2022-06-12 17:25:31 -0700

    implement Property

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,13 +1,16 @@
 import axios from "axios";
+import { loadEnvConfig } from '@next/env'
 
 const API_KEY = process.env.API_KEY
 export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async (url) => {
-    const response = await axios.get((url), {
+    const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Key': `${API_KEY}`,
-                'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     })
+
+    return data;
 }
